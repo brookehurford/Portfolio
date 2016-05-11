f1a83122a30d6da39a2da3ff274fa5b8640ab2a6
Brooke Hurford <brookebhurford@gmail.com>
2016-05-11 14:25:09 -0700

    Add hovers to project grid

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,17 @@ $(document).ready(function(){
   $(".portfolio-content").hide();
   $(".closing-X").hide();
 
-  $(".portfolio-block").click(function(){
+  $(".portfolio-link").click(function(){
     $(".portfolio-block").addClass("full-width");
     $(".portfolio-content").show();
     $(".closing-X").show();
+    $(".contact-block").hide();
   });
 
   $(".closing-X").click(function(){
     $(".portfolio-block").removeClass('full-width');
+    $(".portfolio-content").hide();
+    $(".closing-X").hide();
+    $(".contact-block").show();
   });
 });
